3201b9257d7cf9525f014d9c639bf31de37e2576
Christian Farias Aguila <christian.farias.aguila@gmail.com>
2021-08-11 19:46:05 -0400

    Agrega checkboxes para incluir tipos de caracteres
    - Agrega checkboxes que permiten incluir caracteres alfabéticos, numéricos, o ambos en la contraseña a generar

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
-const caracteresPermitidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const caracteresAlfabeticos = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const caracteresNumericos = "0123456789";
+
+let checkboxLetras = document.getElementById('has-letters')
+let checkboxNumeros = document.getElementById('has-numbers')
 let inputContraseniaGenerada = document.getElementById('result')
 
 document.getElementById('generate').addEventListener('click', () => {
-  inputContraseniaGenerada.value = generarContrasenia();
+  let contieneLetras = checkboxLetras.checked;
+  let contieneNumeros = checkboxNumeros.checked;
+
+  if (!contieneLetras && !contieneNumeros) {
+    alert("Debe seleccionar al menos una opción.");
+    return;
+  }
+
+  inputContraseniaGenerada.value = generarContrasenia(contieneLetras, contieneNumeros);
 });
 
-
-function obtenerCaracterAleatorio() {
+function obtenerCaracterAleatorio(caracteresPermitidos) {
   let index = Math.floor(Math.random() * caracteresPermitidos.length);
   return caracteresPermitidos[index];
 }
 
 
-function generarContrasenia(cantidadCaracteres = 12) {
+function generarContrasenia(contieneLetras, contieneNumeros, cantidadCaracteres = 12) {
   let contrasenia = "";
 
+  // Crear una lista de caracteres permitidos.
+  let caracteresPermitidos = "";
+
+  // Si contiene letras/numeros, agregarlos a la lista.
+  if (contieneLetras) caracteresPermitidos += caracteresAlfabeticos;
+  if (contieneNumeros) caracteresPermitidos += caracteresNumericos;
+
+  // Agregar la cantidad de caracteres aleatorios.
   for (let index = 0; index < cantidadCaracteres; index++) {
-    contrasenia += obtenerCaracterAleatorio();
+    contrasenia += obtenerCaracterAleatorio(caracteresPermitidos);
   }
 
   return contrasenia;
